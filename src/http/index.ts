65f70013ee4ebb06f9451446935149dd1a3d0c36
Anton <antonpaper500@gmail.com>
2024-11-11 00:24:20 +0300

    updated chats

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,7 +1,7 @@
 import { AuthResponse } from "@/models/response/AuthResponse";
 import axios from "axios";
 
-export const API_URL = "http://localhost:4000/api";
+export const API_URL = "https://cfb-rose.vercel.app/api";
 
 const axiosWithAuth = axios.create({
   withCredentials: true,
