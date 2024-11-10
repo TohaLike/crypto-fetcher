570ded5e045b15f786ec88f3a18e90fb2a3211fd
Anton <antonpaper500@gmail.com>
2024-11-11 00:45:15 +0300

    updated chats

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const URL = process.env.NODE_ENV === "production" ? undefined : "http://localhost:4000";
 const token = typeof window !== "undefined" ? localStorage.getItem("token") : null;
 
-export const socket = io("https://cfb-rose.vercel.app/", {
+export const socket = io("https://cfb-antons-projects-5f3e0d01.vercel.app/", {
   transports: ["websocket", "polling"],
   auth: { token: token },
   withCredentials: true,
