2ebb9b7d905e4c33910b1e9ba98ebca0dd0f01e1
Anton <antonpaper500@gmail.com>
2024-11-11 21:54:13 +0300

    updated chats

--- src/components/shared/RegisterPage/RegisterPage.tsx
+++ src/components/shared/RegisterPage/RegisterPage.tsx
@@ -7,13 +7,15 @@ import { redirect, useRouter } from "next/navigation";
 
 export const RegisterPage: React.FC = () => {
   const [openModal, setOpenModal] = useState<boolean>(false);
-  const { isAuthorized, isLoading } = useAuthorized();
+  const { isAuthorized, isLoading, userData } = useAuthorized();
 
   const router = useRouter();
 
   const handleClose = () => setOpenModal(false);
 
   // if (isAuthorized) redirect("/home");
+
+  console.log(userData)
 
   return (
     <div>
