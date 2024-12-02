ce34a88f938fab0d51677efda05446331f748cbf
Anton <antonpaper500@gmail.com>
2024-12-02 17:27:22 +0300

    updated socket

--- src/components/ui/EmojiButton/EmojiButton.tsx
+++ src/components/ui/EmojiButton/EmojiButton.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import EmojiPicker from "emoji-picker-react";
-import { Button } from "@mui/material";
+import { Button, Menu } from "@mui/material";
 import { SmileIcon } from "@/components/icons/smile";
 import emojibutton from "./emojibutton.module.scss";
 
@@ -10,35 +10,76 @@ interface Props {
 }
 
 export const EmojiButton: React.FC<Props> = ({ setMessage }) => {
-  const [open, setOpen] = useState<boolean>(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const getSicker = useMemo(() => {
+    return (
+      <div className={emojibutton.container__emoji}>
+        <EmojiPicker
+          onEmojiClick={(value) => setMessage((prev: any) => prev + value.emoji)}
+          lazyLoadEmojis={false}
+          style={{ borderRadius: "16px" }}
+          skinTonesDisabled
+          searchPlaceHolder="Search"
+          previewConfig={{
+            showPreview: false,
+          }}
+        />
+      </div>
+    );
+  }, [open, setMessage, EmojiPicker]);
 
   return (
     <>
       <div className={emojibutton.container}>
-        <EmojiPicker
-          open={open}
-          onEmojiClick={(value) => setMessage((prev: any) => prev + value.emoji)}
-          lazyLoadEmojis={true}
-          className={emojibutton.container__emoji}
-          width={"300px"}
-          height={"400px"}
-          style={{ position: "absolute", top: "-420px", left: 0 }}
-        />
-
-        <Button
-          onClick={() => setOpen(true)}
+        <Menu
           sx={{
-            "&.MuiButton-root": {
-              p: 0,
-              minWidth: "24px",
-              minHeight: "24px",
-              borderRadius: "16px",
-              color: "#fff",
+            "& .MuiPaper-root": {
+              bgcolor: "transparent",
             },
           }}
+          transformOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          anchorOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
         >
-          <SmileIcon />
-        </Button>
+          {getSicker}
+        </Menu>
+
+        <div>
+          <Button
+            onClick={handleClick}
+            sx={{
+              "&.MuiButton-root": {
+                p: 0,
+                minWidth: "24px",
+                minHeight: "24px",
+                borderRadius: "16px",
+                color: "#fff",
+              },
+            }}
+          >
+            <SmileIcon />
+          </Button>
+        </div>
       </div>
     </>
   );
